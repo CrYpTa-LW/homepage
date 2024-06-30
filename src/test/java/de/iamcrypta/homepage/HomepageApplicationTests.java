a2a72c1caa68a02784a895cc13ab2667580d1830
acp567 <leon.wegener@haw-hamburg.de>
2024-07-01 00:34:33 +0200

    initial spring boot files

---
+++ src/test/java/de/iamcrypta/homepage/HomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iamcrypta.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
