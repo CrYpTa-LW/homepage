e97361833e700010aad3059d1e2179b9d382a027
Leon <leon.wegener98@gmail.com>
2024-07-04 21:57:14 +0000

    add package and class for scheduled task

--- src/main/java/de/iamcrypta/homepage/HomepageApplication.java
+++ src/main/java/de/iamcrypta/homepage/HomepageApplication.java
@@ -2,8 +2,10 @@ package de.iamcrypta.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HomepageApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/de/iamcrypta/homepage/scheduledTask/sooooosScheduledTask.java
@@ -0,0 +1,13 @@
+package de.iamcrypta.homepage.scheduledTask;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class sooooosScheduledTask {
+
+    private
+
+    public void checkForPlaylistUpdates(){
+
+    }
+}
