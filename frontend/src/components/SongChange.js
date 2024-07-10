1b8ebb2de8915d5bd58c26467c2beb1db7f94557
Leon <leon.wegener98@gmail.com>
2024-07-10 21:07:17 +0000

    change date display on frontend

--- frontend/src/components/SongChange.js
+++ frontend/src/components/SongChange.js
@@ -35,9 +35,16 @@ const SongChange = () => {
                     <li key={index} className="list-group-item">
                         <h2 className="h5">{songChange.songName}</h2>
                         <p>Added by: {songChange.addedBy}</p>
-                        <p>Change Occurred At: {new Date(songChange.changeOccurredAt).toLocaleString()}</p>
+                        <p>Change Occurred At: {new Date(songChange.changeOccurredAt).toLocaleDateString('de-DE')}</p>
                         <p>Duration: {Math.floor(songChange.durationMs/1000/60)}:{Math.floor(songChange.durationMs/1000%60)}min </p>
-                        <p>Spotify URL: <a href={songChange.spotifyExternalUrl.match(/spotify=(https:\/\/open.spotify.com\/track\/[^\}]*)/)[1]} target="_blank" rel="noopener noreferrer">Listen</a></p>
+                        <p>
+                            Spotify URL: 
+                            {songChange.spotifyExternalUrl && songChange.spotifyExternalUrl.match(/spotify=(https:\/\/open.spotify.com\/track\/[^\}]*)/) ? (
+                                <a href={songChange.spotifyExternalUrl.match(/spotify=(https:\/\/open.spotify.com\/track\/[^\}]*)/)[1]} target="_blank" rel="noopener noreferrer">Listen</a>
+                            ) : (
+                                'N/A'
+                            )}
+                        </p>
                         <p>Spotify Song ID: {songChange.spotifySongId}</p>
                         <p>Status: <span className={songChange.deleted ? 'badge bg-danger' : 'badge bg-success'}>{songChange.deleted ? 'Deleted' : 'Added'}</span></p>
                     </li>
