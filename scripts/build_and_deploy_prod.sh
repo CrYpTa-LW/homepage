979380a0d7d0f80ef50e39d04f1170ff427a9363
Leon <leon.wegener98@gmail.com>
2024-07-12 15:00:29 +0000

    add build and deploy script

---
+++ scripts/build_and_deploy_prod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "###Docker compose down for webserver_homepage###"
+docker compose --file /home/crypta/dockerContainers/webserver_homepage/docker-compose-fullstack.yml down
+echo "###Done###"
+
+echo "###Building Backend###"
+mvn -f /home/crypta/git/homepage/backend/pom.xmp -Dmaven.test.skip clean install
+echo "###Done###"
+
+echo "###Removing old files###"
+rm /home/crypta/git/homepage/backend/target/homepage-backend-prod.jar
+rm -r /home/crypta/dockerContainers/webserver_homepage/frontend-data/build/
+echo "###Done###"
+
+echo "###Coping new files and rename"
+cp /home/crypta/git/homepage/backend/target/homepage-0.0.1-SNAPSHOT.jar /home/crypta/dockerContainers/webserver_homepage/backend-data/
+mv /home/crypta/dockerContainers/webserver_homepage/backend-data/homepage-0.0.1-SNAPSHOT.jar /home/crypta/dockerContainers/webserver_homepage/backend-data/homepage-backend-prod.jar
+cp -r /home/crypta/git/homepage/frontend/build/ /home/crypta/dockerContainers/webserver_homepage/frontend-data/
+echo "###Done###"
+
+echo "Docker compose up (detach) for webserver_homepage"
+docker compose --file /home/crypta/dockerContainers/webserver_homepage/docker-compose-fullstack.yml up -d
+echo "###Done###"
