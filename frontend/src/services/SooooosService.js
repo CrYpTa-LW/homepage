d70883b7f8cfe6b7fe03f033ac027fbd3a984844
Leon <leon.wegener98@gmail.com>
2024-07-26 23:44:12 +0000

    change frontend url call from dev to prod url

--- frontend/src/services/SooooosService.js
+++ frontend/src/services/SooooosService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-//const SOOOOOS_REST_API_URL_SONGCHANGE = "https://api.leon-wegener.de/api/v1/getSongChange";
-const SOOOOOS_REST_API_URL_SONGCHANGE = "http://192.168.178.90:8082/api/v1/getSongChange";
-//const SOOOOOS_REST_API_URL_PLAYLIST_STATS = "https://api.leon-wegener.de/api/v1/getPlaylistStatsForDuration";
-const SOOOOOS_REST_API_URL_PLAYLIST_STATS = "http://192.168.178.90:8082/api/v1/getPlaylistStatsForDuration";
+const SOOOOOS_REST_API_URL_SONGCHANGE = "https://api.leon-wegener.de/api/v1/getSongChange";
+//const SOOOOOS_REST_API_URL_SONGCHANGE = "http://192.168.178.90:8082/api/v1/getSongChange";
+const SOOOOOS_REST_API_URL_PLAYLIST_STATS = "https://api.leon-wegener.de/api/v1/getPlaylistStatsForDuration";
+//const SOOOOOS_REST_API_URL_PLAYLIST_STATS = "http://192.168.178.90:8082/api/v1/getPlaylistStatsForDuration";
 
 class SooooosService {
     constructor(){}
